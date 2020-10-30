2627d2aa3afd8f023a9caa3f544126dbeab28163
migueldossantos011@gmail.com <migueldossantos011@gmail.com>
2020-10-30 20:33:53 -0300

    :package: Adicionando projeto kotlin no repositório :man_technologist:

---
+++ ByteBank/src/main.kt
@@ -0,0 +1,25 @@
+fun main() {
+    println("Bem vindo ao bytebank")
+    val titular: String = "Miguel"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+
+    saldo = 100 + 2.0
+    saldo += 200
+    saldo -= 1000.0
+
+    println("Titular: $titular")
+    println("Número da conta $numeroConta")
+    println("Saldo da conta $saldo")
+
+    if (saldo > 0.0) {
+        println("Conta é positiva")
+    } else if (saldo == 0.0) {
+        println("Conta é neutra")
+    } else if(saldo < 0.0) {
+        println("Conta é negativa")
+    } else {
+        println("Erro interno")
+    }
+
+}
